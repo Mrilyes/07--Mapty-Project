78eb353edad921da30d114d9827a3a923c2a51b6
Mrilyes <ilyesghlila1.gmail.com>
2022-06-22 23:06:51 +0100

    displaying map marker and rendering workout input form

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent;
+
 // geolocation
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
@@ -25,18 +27,54 @@ if (navigator.geolocation)
 
       const coords = [latitude, longitude];
 
-      const map = L.map('map').setView(coords, 13);
+      map = L.map('map').setView(coords, 15);
+      // console.log(map);
       L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         // maxZoom: 19,
         attribution: '© OpenStreetMap',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup('<b>Hello world!</b><br>I am a popup.')
-        .openPopup();
+      // the on() method is generated from leaflet library it replace the event listener
+      map.on('click', function (mapE) {
+        // I copy mapE to the global variable to get access to the next event listener
+        mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus();
+      });
     },
     function () {
       alert('Could not get your position , allow location access');
     }
   );
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  // clear input fields
+  inputDistance.value =
+    inputDuration.value =
+    inputCadence.value =
+    inputElevation.value =
+      '';
+
+  // display the marker
+  console.log(mapEvent);
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('Workout')
+    .openPopup();
+});
+// if type is cycling
+inputType.addEventListener('change', function (e) {
+  e.preventDefault();
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
